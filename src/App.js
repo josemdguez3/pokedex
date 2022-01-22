f098f4c75b13f1e1dc874a50daba5f13f4924eaa
josemdguez3 <josemdguez3@gmail.com>
2022-01-22 15:03:28 +0000

    Installed React Semantic UI
    Installed semantic-ui-react and semantic-ui-css

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Hello, World! My name is Jose!
-        </p>
+        </h1>
         <p> 
           ;)
         </p>
